13e346862249bb6444e445f7d8b538ebd31fb085
juzoqqe <146557501+juzoqqe@users.noreply.github.com>
2026-03-01 18:21:19 +0100

    Refactor database connection to use environment variable

--- server/db.js
+++ server/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "artplatform",
-  password: "2812",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
